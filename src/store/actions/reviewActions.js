dda83d7a8f1c42bd4e7b687c539468a9a7b11e46
Diana Cordun <77334927+dianacordun@users.noreply.github.com>
2023-01-26 22:06:25 +0200

    adding reviews

--- src/store/actions/reviewActions.js
+++ src/store/actions/reviewActions.js
@@ -1,6 +1,17 @@
 export const createReview = (review) => {
-    return (dispatch, getState, { getFirebase, getFirestore }) => {
-      // make async call to database
-      dispatch({ type: 'CREATE_REVIEW', review });
-    }
-  };
+return (dispatch, getState, {getFirestore}) => {
+    // make async call to database
+    const firestore = getFirestore();
+    firestore.collection('reviews').add({
+    ...review,
+    authorFirstName: 'alex',
+    authorLastName: 'ion',
+    authorId: 12345,
+    createdAt: new Date()
+    }).then(() => {
+    dispatch({ type: 'CREATE_REVIEW_SUCCESS' });
+    }).catch(err => {
+    dispatch({ type: 'CREATE_REVIEW_ERROR' }, err);
+    });
+}
+};
--- src/store/reducers/reviewReducer.js
+++ src/store/reducers/reviewReducer.js
@@ -5,12 +5,17 @@ const initState = {
         {id: '3', title:'review3', content:'aaaaaaaaaaaaaaaaa'}
     ]
 };
-const reviewReducer = (state = initState, action) => {
-    switch (action.type) {
-      case 'CREATE_REVIEW':
-        console.log('create review', action.review);
-    }
-    return state;
-  };
 
+const reviewReducer = (state = initState, action) => {
+switch (action.type) {
+    case 'CREATE_REVIEW_SUCCESS':
+        console.log('create review success');
+        return state;
+    case 'CREATE_REVIEW_ERROR':
+        console.log('create review error');
+        return state;
+    default:
+        return state;
+}
+};
 export default reviewReducer
